ebf9033e6a882003b41331f4522c475eda76d465
fisher <goshurenkopl@gmail.com>
2020-11-29 14:13:35 +0200

    add rotation

--- product_app/models.py
+++ product_app/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 import uuid
+import time
+from PIL import Image as Img
+from io import BytesIO
+from django.core.files.base import ContentFile
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
@@ -12,3 +16,15 @@ class Product(models.Model):
     rotate_duration = models.FloatField(
         null=True, blank=True
         )
+    modified = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        if self.logo:
+            image = Img.open(self.logo)
+            start_time = time.time()
+            image = image.rotate(180)
+            self.rotate_duration = time.time() - start_time
+            image_io = BytesIO()
+            image.save(image_io, format='jpeg', quality=80)
+            self.logo = ContentFile(image_io.getvalue(), 'logo.jpg')
+        super().save(*args, **kwargs)
